9bde8dd0a99eb0b1474937334b73740d91abd826
Michael Cheng <mcheng.work@gmail.com>
2017-01-22 15:33:19 +0800

    With basic Upload module support

--- ansible/scripts/install_nginx_uploader.sh
+++ ansible/scripts/install_nginx_uploader.sh
@@ -6,13 +6,15 @@ mkdir ~/working && cd ~/working
 
 wget https://nginx.org/download/nginx-1.11.8.tar.gz
 wget -O nginx-upload-module.zip https://github.com/Austinb/nginx-upload-module/archive/2.2.zip
+wget -O nginx-upload-progress-module.zip https://github.com/masterzen/nginx-upload-progress-module/archive/master.zip
 
 tar -zxvf nginx-1.11.8.tar.gz
 unzip nginx-upload-module.zip
+unzip nginx-upload-progress-module.zip
 
 cd nginx-1.11.8
 
-sudo ./configure --prefix=/opt/nginx --with-http_ssl_module --add-module=../nginx-upload-module-2.2
+sudo ./configure --prefix=/opt/nginx --with-http_ssl_module --add-module=../nginx-upload-module-2.2 --add-module=../nginx-upload-progress-module-master
 
 sudo make
 sudo make install
