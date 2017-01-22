9539529466446fc59db97fd6676283e824a109ae
Michael Cheng <mcheng.work@gmail.com>
2017-01-22 15:59:52 +0800

    Proper response page.

--- html/test.php
+++ html/test.php
@@ -1,3 +1,13 @@
 <?php
 error_log(print_r($_POST, true));
 ?>
+<html>
+  <head>
+    <title>Uploaded</title>
+  </head>
+  <body>
+    <h1>Uploaded files:</h1>
+    <pre><?php print_r($_POST); ?></pre>
+    <a href="/">&laquo; Back</a>
+  </body>
+</html>
