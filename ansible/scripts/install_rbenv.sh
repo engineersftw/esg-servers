8dc636e4602509b171a888424fdacee9dac176cd
Michael Cheng <mcheng.work@gmail.com>
2017-02-19 13:48:28 +0800

    Install Ruby and Passenger

---
+++ ansible/scripts/install_rbenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+
+# source ~/.bashrc
+
+# rbenv install 2.4.0 && rbenv global 2.4.0
