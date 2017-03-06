c440dc39e8ba96ccb05558fb69f7b3a3bf1b7f91
Michael Cheng <mcheng.work@gmail.com>
2017-03-07 01:26:52 +0800

    Split publishing workers into a separate Publishing queue.

--- video_uploader/app/workers/upload_video_worker.rb
+++ video_uploader/app/workers/upload_video_worker.rb
@@ -1,5 +1,6 @@
 class UploadVideoWorker
   include Sidekiq::Worker
+  sidekiq_options queue: :publishing
 
   def perform(presentation_id, video_file)
     presentation = Presentation.find(presentation_id)
