0584301dadfb539b86779a97a82d13a7e63280c9
Michael Cheng <mcheng.work@gmail.com>
2017-03-07 23:07:04 +0800

    Specific badge colours for different statuses

--- video_uploader/app/helpers/presentations_helper.rb
+++ video_uploader/app/helpers/presentations_helper.rb
@@ -1,2 +1,21 @@
 module PresentationsHelper
+  def self.badge_color(presentation)
+    case presentation.status
+      when 'failed_to_process'
+      when 'failed_to_publish'
+      when 'failed_to_add_to_playlist'
+        'red darken-1'
+
+      when 'processing'
+      when 'publishing_to_youtube'
+        'orange lighten-2'
+
+      when 'published_to_youtube'
+      when 'adding_to_playlist'
+        'green darken-1'
+
+      else
+        'blue darken-1'
+    end
+  end
 end
