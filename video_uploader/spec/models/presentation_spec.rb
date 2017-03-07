28462b7d99c1adc8852fb23be6fcd1e5b81cacc3
Michael Cheng <mcheng.work@gmail.com>
2017-03-07 10:31:36 +0800

    Added validation for the model.

--- video_uploader/spec/models/presentation_spec.rb
+++ video_uploader/spec/models/presentation_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Presentation, type: :model do
+  describe '#validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:presented_at) }
+  end
+
   describe '#needs_video?' do
     context 'will needs video' do
       before do
