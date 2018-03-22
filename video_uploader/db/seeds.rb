8bb6f40a8ae6bc77488caacaf1594efd1c71e503
Michael Cheng <mcheng.work@gmail.com>
2018-03-22 09:10:40 +0800

    Use new playlist info for FOSSASIA 2018

--- video_uploader/db/seeds.rb
+++ video_uploader/db/seeds.rb
@@ -9,10 +9,10 @@
 Admin.create!(email: 'upload@engineers.sg', password: 'password1234', password_confirmation: 'password1234')
 
 # Engineers.SG
-Playlist.create(title: 'Meetup Videos', playlist_uid: 'PLECEw2eFfW7hYMucZmsrryV_9nIc485P1', playlist_source: 'youtube')
+# Playlist.create(title: 'Meetup Videos', playlist_uid: 'PLECEw2eFfW7hYMucZmsrryV_9nIc485P1', playlist_source: 'youtube')
 
 # FOSSASIA 2017
-# Playlist.create(title: 'FOSSASIA 2017', playlist_uid: 'PLzZVLecTsGpLhEwJlgVLP-36T7b8AjRG1', playlist_source: 'youtube')
+Playlist.create(title: 'FOSSASIA 2018', playlist_uid: 'PLzZVLecTsGpKb1P8J5WLrONMLby5Dn1EV', playlist_source: 'youtube')
 
 # Michael Cheng
 # Playlist.create(title: 'Test Playlist', playlist_uid: 'PLyyvDe8MX3xlcdaNAhoMgc2hAl5HAntTi', playlist_source: 'youtube')
